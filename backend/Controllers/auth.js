72623ded1cb63e69835fa93e9347a664572965ae
Chiranjeevi Tirunagari <vchiranjeeviak.tirunagari@gmail.com>
2021-12-31 09:44:38 +0530

    added user signup functionality

--- backend/Controllers/auth.js
+++ backend/Controllers/auth.js
@@ -1,5 +1,23 @@
-exports.signup = (req, res) => {
+const { validationResult } = require('express-validator');
+const User = require('../Models/User');
 
+exports.signup = (req, res) => {
+    const errors = validationResult(req.body);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            error: errors
+        });
+    }
+    let user = new User(req.body);
+
+    user.save((err, user) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            })
+        }
+        return res.status(200).json(user);
+    })
 }
 
 exports.signin = (req, res) => {
--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 const { signup, signin, signout } = require('../Controllers/auth');
 
-router.post('/signup', signup);
+router.post('/signup', [
+    check('firstName').isLength({ min: 1, max: 40 }).withMessage('First Name should be > 0 and <= 40 characters'),
+    check('lastName').isLength({ min: 1, max: 40 }).withMessage('Last Name should be > 0 and <= 40 characters'),
+    check('email').isEmail().withMessage('Enter a valid email address'),
+    check('password').isLength({ min: 8 }),
+    check('phoneNumber').isLength({ min: 10, max: 10 }).withMessage('Enter a valid phone number')
+], signup);
+
 router.post('/signin', signin);
+
 router.get('/signout', signout);
 
 module.exports = router;
