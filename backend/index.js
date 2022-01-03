0916e37013dbed5710d48ec42c986aa1ad6a0056
Chiranjeevi Tirunagari <vchiranjeeviak.tirunagari@gmail.com>
2022-01-03 15:43:12 +0530

    install cors, added as middleware.

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cookieParser = require("cookie-parser")
 const mongoose = require('mongoose');
+const cors = require('cors');
 const authRoutes = require('./Routes/auth');
 const userRoutes = require('./Routes/user');
 const productRoutes = require('./Routes/product');
@@ -24,6 +25,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
